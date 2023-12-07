8a3706d39159921fbd69c1208e3f3c1c3633272f
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2023-12-07 11:49:34 -0300

    criação da segunda activity

---
+++ app/src/main/java/com/example/disp_moveis_3/TaskCreateActivity.java
@@ -0,0 +1,14 @@
+package com.example.disp_moveis_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_create);
+    }
+}
